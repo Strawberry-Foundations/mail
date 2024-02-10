b9daa70b50b1defe111d93816782c4541a221e54
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 21:14:17 +0000

    Added __init__ for Config

--- mail/core/config.py
+++ mail/core/config.py
@@ -4,14 +4,15 @@ import yaml
 with open(server_dir + "/config.yml", "r") as _config:
     config = yaml.load(_config, Loader=yaml.SafeLoader)
 
+static_url_path = config["vars"]["static_url_path"]
+static_folder = config["vars"]["static_folder"]
+template_folder = config["vars"]["template_folder"]
+
 
 class Config:
-    address = config["host"]["address"]
-    port = config["host"]["port"]
+    def __init__(self):
+        self.address = config["host"]["address"]
+        self.port = config["host"]["port"]
 
-    debug_mode = config["flags"]["debug_mode"]
-    threaded = config["flags"]["threaded"]
-
-    static_url_path = config["vars"]["static_url_path"]
-    static_folder = config["vars"]["static_folder"]
-    template_folder = config["vars"]["template_folder"]
+        self.debug_mode = config["flags"]["debug_mode"]
+        self.threaded = config["flags"]["threaded"]
