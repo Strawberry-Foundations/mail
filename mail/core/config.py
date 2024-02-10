6b351a615fa66e6fa5d894eccf0e15c098939c00
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 19:29:48 +0000

    Added address, port, debug_mode + threaded config

--- mail/core/config.py
+++ mail/core/config.py
@@ -6,6 +6,12 @@ with open(server_dir + "/config.yml", "r") as _config:
 
 
 class Config:
+    address = config["host"]["address"]
+    port = config["host"]["port"]
+
+    debug_mode = config["flags"]["debug_mode"]
+    threaded = config["flags"]["threaded"]
+
     static_url_path = config["vars"]["static_url_path"]
     static_folder = config["vars"]["static_folder"]
     template_folder = config["vars"]["template_folder"]
