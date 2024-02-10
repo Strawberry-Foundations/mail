2b46771006f171288b412d4baa8445dbd7b6a62b
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 19:14:51 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='strawberry-mail',
+    version='1.0.0',
+    packages=['mail'],
+    url='',
+    license='MIT',
+    author='Juliandev02',
+    author_email='julian@strawberryfoundations.xyz',
+    description='Strawberry Mail Server'
+)
