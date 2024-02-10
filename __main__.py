3983829ed30d29df4a00bf8d7542d6f3e12a0b54
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 19:29:52 +0000

    Added address, port, debug_mode + threaded config

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
 from mail.core.app import app
+from mail.core.config import Config
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=Config.address, port=Config.port, debug=Config.debug_mode, threaded=Config.threaded)
+    
