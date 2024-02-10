a46cfafdb57fe6f6a9bb88796933fb8f13a4e0f8
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-10 21:14:09 +0000

    Fixed config + updated App builder

--- mail/core/app.py
+++ mail/core/app.py
@@ -2,15 +2,17 @@ from flask import Flask
 
 from mail.core.config import *
 from mail.utils.utilities import load_secret
+from mail.utils.colors import *
 
 from mail.routes.index import index
 
+import os
 
 app = Flask(
     __name__,
-    static_url_path=Config.static_url_path,
-    static_folder=Config.static_folder,
-    template_folder=Config.template_folder
+    static_url_path=static_url_path,
+    static_folder=static_folder,
+    template_folder=template_folder
 )
 
 app.config["SECRET_KEY"] = load_secret()
@@ -22,11 +24,20 @@ class App:
     def __init__(self, flask_app: Flask):
         self.app = flask_app
 
-    def setup(self):
-        print("   Thanks for using Strawberry Mail Server!")
-        print(f"   Root Path: {self.app.root_path}")
+    def run(self, app_config: Config):
+        if not app_config.debug_mode:
+            combined_path = os.path.join(self.app.root_path, self.app.template_folder)
+            absolute_path = os.path.abspath(combined_path)
 
-        print("")
+            print(f"   {GREEN}{BOLD}{UNDERLINE}Thanks for using Strawberry Mail Server!{CRESET}")
+            print(f"   {BLUE}{BOLD}Root Path:{CRESET} {self.app.root_path}")
+            print(f"   {BLUE}{BOLD}Template Path:{CRESET} {absolute_path}")
 
-    def run(self):
-        self.app.run(host=Config.address, port=Config.port, debug=Config.debug_mode, threaded=Config.threaded)
+            print("")
+
+        self.app.run(
+            host=app_config.address,
+            port=app_config.port,
+            debug=app_config.debug_mode,
+            threaded=app_config.threaded
+        )
