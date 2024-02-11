f84dab1aa5421d5800114aeba02f3eb383b5fc7b
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 16:26:46 +0100

    Fixed config

--- mail/core/config.py
+++ mail/core/config.py
@@ -2,17 +2,24 @@ from mail import server_dir
 import yaml
 
 with open(server_dir + "/config.yml", "r") as _config:
-    config = yaml.load(_config, Loader=yaml.SafeLoader)
+    config_data = yaml.load(_config, Loader=yaml.SafeLoader)
 
-static_url_path = config["vars"]["static_url_path"]
-static_folder = config["vars"]["static_folder"]
-template_folder = config["vars"]["template_folder"]
+static_url_path = config_data["vars"]["static_url_path"]
+static_folder = config_data["vars"]["static_folder"]
+template_folder = config_data["vars"]["template_folder"]
 
 
 class Config:
     def __init__(self):
-        self.address = config["host"]["address"]
-        self.port = config["host"]["port"]
+        self.address = config_data["host"]["address"]
+        self.port = config_data["host"]["port"]
 
-        self.debug_mode = config["flags"]["debug_mode"]
-        self.threaded = config["flags"]["threaded"]
+        self.debug_mode = config_data["flags"]["debug_mode"]
+        self.threaded = config_data["flags"]["threaded"]
+
+        self.mail_tld = config_data["vars"]["mail_tld"]
+        self.static_url_path = config_data["vars"]["static_url_path"]
+        self.static_folder = config_data["vars"]["static_folder"]
+        self.template_folder = config_data["vars"]["template_folder"]
+
+config = Config()
