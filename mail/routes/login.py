d9ebfddab81bd3d3f17c16e1c36cafbda2771a26
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 15:12:16 +0100

    Fixed Sid Login

--- mail/routes/login.py
+++ mail/routes/login.py
@@ -1,10 +1,26 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from mail.core.locale import get_preferred_language, Strings
+from mail import STRAWBERRY_ID_URL
+import requests
 
 
 async def login():
     lang = get_preferred_language()
     strings = Strings(lang)
+    redir_lang = "en"
+
+    if "strawberry_id_auth" in request.args:
+        match lang:
+            case "de-DE":
+                redir_lang = "de"
+            case "en-US":
+                redir_lang = "en"
+
+        return redirect(f"{STRAWBERRY_ID_URL}{redir_lang}/login/?redirect=0.0.0.0:8080")
+
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
 
     return render_template("login.html", **{
         "title": strings.load("title_welcome"),
