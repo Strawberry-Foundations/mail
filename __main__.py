8440ac73ffcdc5010d4f35bd2ec03a1dbfd6dac6
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 15:12:54 +0100

    Some changes to config

--- __main__.py
+++ __main__.py
@@ -1,7 +1,5 @@
-from mail.core.app import App, app
-from mail.core.config import Config
+from mail.core.app import App, app, config
 
-config = Config()
 runtime = App(app)
 
 if __name__ == "__main__":
