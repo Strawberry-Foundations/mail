c3e0bd824d4d5c192517e6acadc3628e82614ff8
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-12 00:02:40 +0100

    Some fixes

--- mail/routes/user/webmail.py
+++ mail/routes/user/webmail.py
@@ -1,6 +1,7 @@
 from flask import render_template, session
 
 from mail.auth import require_login
+from mail.core.logger import logger
 from mail.core.locale import get_preferred_language, Strings
 from mail.imap.client import get_imap, ImapServerException, get_all_emails
 
@@ -10,13 +11,14 @@ async def webmail():
     lang = get_preferred_language()
     strings = Strings(lang)
 
+    user_email = session.get("auth.email")
+    password = session.get("auth.password")
+
     try:
         imap = get_imap().test_connection()
 
-    except ImapServerException:
-        user_email = session.get("auth.email")
-        password = session.get("auth.password")
-
+    except ImapServerException as err:
+        logger.log(err)
         imap = get_imap()
         imap.connect()
         imap.login(user_email, password)
@@ -26,5 +28,6 @@ async def webmail():
     return render_template("user/webmail.html", **{
         "emails": emails,
         "strings": strings,
-        "title": f"{strings.load('inbox')} - {session.get('auth.email')}"
+        "title": f"{strings.load('inbox')} - {session.get('auth.email')}",
+        "user_mail": user_email,
     })
