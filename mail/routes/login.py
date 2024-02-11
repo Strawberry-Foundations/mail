634a89a115de8e1aaa994dd429d6306a4aba070c
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 00:18:25 +0000

    POST method for login page

--- mail/routes/login.py
+++ mail/routes/login.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template, request
 
 
 async def login():
+    accept_language = request.headers.get('Accept-Language')
+
     return render_template("login.html", **{
-        "title": "Willkommen bei Strawberry Mail"
+        "title": "Willkommen bei Strawberry Mail",
+        "lang": accept_language,
     })
