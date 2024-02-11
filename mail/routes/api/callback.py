97582c19b7415d784eabc59110e53e7b541f09b0
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 16:26:42 +0100

    Fixed callback

--- mail/routes/api/callback.py
+++ mail/routes/api/callback.py
@@ -1,7 +1,6 @@
-from flask import redirect, request
+from flask import redirect, request, session
 from mail import STRAWBERRY_ID_URL
-from mail.core.app import config
-
+from mail.core.config import config
 import requests
 
 
@@ -9,22 +8,24 @@ async def callback():
     redir_lang = request.args.get("hl", default="en")
 
     if "code" not in request.args:
-        return redirect("/login")
+        return redirect("/login?err=no_code")
 
     data = requests.get(f"{STRAWBERRY_ID_URL}api/callback?code={request.args['code']}").json()
 
-    status = data["data"]["status"]
-
-    print(status)
-
-    if status == "Invalid code":
-        return redirect("/login")
+    if data["data"]["status"] == "Invalid code":
+        return redirect("/login?err=invalid_code")
 
     user_data = data["data"]["user"]
 
     email: str = user_data["email"]
-
     email_tld = email.split("@")[1]
 
-    print(email)
-    print(email_tld)
+    if not email_tld.rstrip() == config.mail_tld:
+        return redirect("/login?err=not_permitted")
+
+    session["_strawberryid.username"] = user_data["username"]
+    session["_strawberryid.email"] = user_data["email"]
+    session["_strawberryid.full_name"] = user_data["full_name"]
+    session["_strawberryid.avatarurl"] = user_data["profile_picture_url"]
+
+    return redirect("/login?sid_success")
