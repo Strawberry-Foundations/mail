24cb7e1398a8ded262dadb47033cca4d7c776751
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 23:58:38 +0100

    renamed change_tab to webmail_tab

--- mail/static/js/change_tab.js
+++
@@ -1,9 +0,0 @@
-function change_tab(tabId) {
-  var tabContents = document.querySelectorAll('.tab-content');
-  tabContents.forEach(function(content) {
-    content.classList.remove('active-tab');
-  });
-
-  var selectedTab = document.getElementById(tabId);
-  selectedTab.classList.add('active-tab');
-}
