f7591937cc2cfabb7b6001f5e46ccaff31edf443
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 23:30:50 +0100

    added tabs/sections

--- mail/routes/user/webmail.py
+++ mail/routes/user/webmail.py
@@ -1,11 +1,15 @@
 from flask import render_template, session
 
 from mail.auth import require_login
+from mail.core.locale import get_preferred_language, Strings
 from mail.imap.client import get_imap, ImapServerException, get_all_emails
 
 
 @require_login
 async def webmail():
+    lang = get_preferred_language()
+    strings = Strings(lang)
+
     try:
         imap = get_imap().test_connection()
 
@@ -19,6 +23,8 @@ async def webmail():
 
     emails = get_all_emails(imap.connection)
 
-    return render_template("user/dashboard.html", **{
-        "emails": emails
+    return render_template("user/webmail.html", **{
+        "emails": emails,
+        "strings": strings,
+        "title": f"{strings.load('inbox')} - {session.get('auth.email')}"
     })
