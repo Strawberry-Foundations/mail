0f914251e746668c4107eba1c30b4566a88af4f7
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-11 17:49:41 +0100

    Added better logging

--- mail/core/logger.py
+++ mail/core/logger.py
@@ -1,15 +1,20 @@
 from mail.utils.colors import *
 from datetime import datetime
+from enum import Enum
+
+
+class LogType(Enum):
+    UserManager = f"[{BLUE}{BOLD}UManager{CRESET}]    "
 
 
 class Logger:
     def __init__(self):
         self.logger = self
 
-    def log(self, text):
+    def log(self, text, log_type: LogType = LogType.UserManager):
         timestamp_string = datetime.now().strftime("%d/%b/%Y %H:%M:%S")
 
-        print(f"[{GREEN}{timestamp_string}{CRESET}] {text}")
+        print(f"{log_type.value}[{GREEN}{timestamp_string}{CRESET}] {text}")
 
 
 logger = Logger()
