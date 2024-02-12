98c064c1670fd9d075838f73f463e86534bec8bb
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-12 14:33:14 +0100

    Updated errors

--- mail/err/imap.py
+++ mail/err/imap.py
@@ -1,4 +1,23 @@
 class ImapFetchException(Exception):
-    def __init__(self, message="Connection error"):
+    def __init__(self, message="Exception while fetching emails"):
         self.message = message
         super().__init__(self.message)
+
+
+class ImapMailBoxSelectException(Exception):
+    def __init__(self, message="Exception while selecting mail box"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ImapFetchMailBoxException(Exception):
+    def __init__(self, message="Exception while fetching mail boxes"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class ImapStatusException(Exception):
+    def __init__(self, message="Exception while fetching status"):
+        self.message = message
+        super().__init__(self.message)
+
