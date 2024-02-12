9deb1dad7e8b61f496c2e6fba0004f8a73a7dc5e
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-12 01:41:19 +0100

    added extract_name_from_email()

--- mail/utils/utilities.py
+++ mail/utils/utilities.py
@@ -1,5 +1,12 @@
 from mail import server_dir
+import re
+
 
 def load_secret():
     with open(server_dir + "/secret.key", "r") as _secret:
         return _secret.read()
+
+
+def extract_name_from_email(email_address):
+    match = re.search(r'^(.*?)\s*<', email_address)
+    return match.group(1).strip() if match else None
