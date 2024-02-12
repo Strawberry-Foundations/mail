baa379931493c6f926723251978ac295befac62a
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-12 15:12:04 +0100

    added fetch_flagged_mail()

--- mail/imap/mail.py
+++ mail/imap/mail.py
@@ -79,6 +79,54 @@ class Email:
         except Exception as err:
             raise ImapMailBoxSelectException(f"Error while selecting mail box '{folder.value}': {err}")
 
+    def fetch_flagged_mail(self):
+        self.connection.select("INBOX")
+        status, messages = self.connection.search(None, 'FLAGGED')
+
+        if status == 'OK':
+            email_ids = messages[0].split()
+            emails = []
+
+            for email_id in email_ids:
+                status, msg_data = self.connection.fetch(email_id, '(RFC822)')
+
+                if status == "OK":
+                    msg = message_from_bytes(msg_data[0][1])
+
+                    subject, subject_encoding = decode_header(msg['Subject'])[0]
+                    sender, sender_encoding = decode_header(msg.get('From'))[0]
+                    receiver, sender_encoding = decode_header(msg.get('To'))[0]
+
+                    mail_date = parsedate_to_datetime(msg.get('Date')).strftime('%Y-%m-%d %H:%M:%S')
+
+                    imap_date = datetime.strptime(mail_date, "%Y-%m-%d %H:%M:%S")
+                    is_today = imap_date.date() == date.today()
+
+                    if is_today:
+                        mail_date = imap_date.strftime("%H:%M")
+                    else:
+                        mail_date = imap_date.strftime("%d. %b.")
+
+                    subject = subject.decode(subject_encoding or 'utf-8') if subject_encoding else subject
+                    sender = sender.decode(sender_encoding or 'utf-8') if sender_encoding else sender
+
+                    sender_name = extract_name_from_email(sender).rstrip().strip('"')
+                    receiver_name = extract_name_from_email(receiver).rstrip().strip('"')
+
+                    email_info = {
+                        'id': email_id,
+                        'subject': subject,
+                        'sender': sender,
+                        'sender_name': sender_name,
+                        'receiver': receiver,
+                        'receiver_name': receiver_name,
+                        'date': mail_date
+                    }
+
+                    emails.append(email_info)
+
+            return emails
+
     def get_mailboxes(self):
         try:
             status, mailbox_list = self.connection.list()
