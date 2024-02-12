0c7d193c9f503eba3b4a3c1948a6c4fe43943656
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-12 14:32:48 +0100

    Fixed mail boxes

--- mail/routes/user/webmail.py
+++ mail/routes/user/webmail.py
@@ -3,7 +3,8 @@ from flask import render_template, session
 from mail.auth import require_login
 from mail.core.logger import logger
 from mail.core.locale import get_preferred_language, Strings
-from mail.imap.client import get_imap, ImapServerException, get_all_emails
+from mail.imap.client import get_imap, ImapServerException, get_all_emails, get_sent_emails
+from mail.imap.mail import Email, MailBox
 
 
 @require_login
@@ -23,10 +24,20 @@ async def webmail():
         imap.connect()
         imap.login(user_email, password)
 
-    emails = get_all_emails(imap.connection)
+    email = Email(imap.connection)
+
+    mailboxes = email.get_mailboxes()
+    inbox_emails = email.fetch_mails(MailBox.INBOX)
+    sent_emails = email.fetch_mails(MailBox.SENT)
+    drafts_emails = email.fetch_mails(MailBox.DRAFTS)
+    trash_emails = email.fetch_mails(MailBox.TRASH)
 
     return render_template("user/webmail.html", **{
-        "emails": emails,
+        "mailboxes": mailboxes,
+        "inbox_emails": inbox_emails,
+        "sent_emails": sent_emails,
+        "drafts_emails": drafts_emails,
+        "trash_emails": trash_emails,
         "strings": strings,
         "title": f"{strings.load('inbox')} - {session.get('auth.email')}",
         "user_mail": user_email,
